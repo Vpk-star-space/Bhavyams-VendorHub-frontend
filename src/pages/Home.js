9c96b701732beb74177542ae4c31917e80633dc8
Vnkata Pavan Kumar <pavanvenkata63@gmail.com>
2026-04-13 10:29:45 +0530

    footer change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -151,7 +151,7 @@ const Home = () => {
                     Contact: <a href="mailto:pavanvenkat63@gmail.com" style={styles.footerLink}>pavanvenkat63@gmail.com</a>
                 </p>
                 <p style={styles.footerContact}>
-                    Check out our other app: <a href="https://subhams-vpk.vercel.app/" target="_blank" rel="noopener noreferrer" style={styles.footerLink}>Subhams App</a>
+                    Check out our other app: <a href="https://subhams-vpk.vercel.app/" target="_blank" rel="noopener noreferrer" style={styles.footerLink}>Subhams </a>
                 </p>
             </footer>
         </div>
